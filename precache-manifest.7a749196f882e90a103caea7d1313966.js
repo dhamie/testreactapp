8cae286c27fc8937b3a21eb5b5aa0277beb20f99
Soyemi Damilola <dhamie.soyemi@gmail.com>
2019-04-19 19:35:15 +0100

    Updates

---
+++ precache-manifest.7a749196f882e90a103caea7d1313966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dhamie/testreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a217878855e498e9e727",
+    "url": "/dhamie/testreactapp/static/js/runtime~main.6d14bbe9.js"
+  },
+  {
+    "revision": "079fcbdddc82a9234c3f",
+    "url": "/dhamie/testreactapp/static/js/main.27b46123.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/dhamie/testreactapp/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "079fcbdddc82a9234c3f",
+    "url": "/dhamie/testreactapp/static/css/main.55ecca80.chunk.css"
+  },
+  {
+    "revision": "fe0543b35c0c7c3264238da52fd9aad8",
+    "url": "/dhamie/testreactapp/index.html"
+  }
+];
